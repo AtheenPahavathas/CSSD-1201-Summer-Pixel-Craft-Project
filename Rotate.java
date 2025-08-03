646d2c3341b355d745fa2e1ec167e0efc38660ba
Efua Umane <efuaumane79@gmail.com>
2025-08-03 00:26:00 -0400

    Update Rotate.java

--- Rotate.java
+++ Rotate.java
@@ -1,1 +1,6 @@
 // add rotate
+ import java.io.IOException;
+ import java.lang.reflect.InvocationTargetException;
+ import javax.imageio.ImageIO;
+ import java.awt.image.BufferedImage;
+ import java.io.File;
